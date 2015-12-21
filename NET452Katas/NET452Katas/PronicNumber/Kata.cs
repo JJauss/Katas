4a6763c7a381be48d33b07792abb452ff51cdd39
Joachim Jauß <joachim@jauss-web.de>
2015-12-21 14:26:03 +0100

    Clever PronicNumber

--- NET452Katas/NET452Katas/PronicNumber/Kata.cs
+++ NET452Katas/NET452Katas/PronicNumber/Kata.cs
@@ -1,19 +1,13 @@
-﻿namespace NET452Katas.PronicNumber
+﻿using System;
+
+namespace NET452Katas.PronicNumber
 {
 	public class Kata
 	{
 		public static bool IsPronic(int n)
 		{
-
-			int x = 0, p = 0;
-			bool result = false;
-			while (!result && p <= n)
-			{
-				p = x * (x + 1);
-				x++;
-				result = p == n;
-			}
-			return result;
+			int value = (int)Math.Sqrt(n);
+			return (value * (value + 1)) == n;
 		}
 	}
 }
