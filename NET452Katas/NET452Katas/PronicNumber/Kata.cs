dad72d9d26666c5e30b9645de597d1fdae1be4d9
Joachim Jauß <joachim@jauss-web.de>
2015-12-21 14:24:13 +0100

    Process the PronicNumberKata

---
+++ NET452Katas/NET452Katas/PronicNumber/Kata.cs
@@ -0,0 +1,19 @@
+﻿namespace NET452Katas.PronicNumber
+{
+	public class Kata
+	{
+		public static bool IsPronic(int n)
+		{
+
+			int x = 0, p = 0;
+			bool result = false;
+			while (!result && p <= n)
+			{
+				p = x * (x + 1);
+				x++;
+				result = p == n;
+			}
+			return result;
+		}
+	}
+}
---
+++ NET452Katas/NET452Katas/PronicNumber/PronicNumberTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NET452Katas.PronicNumber {
+	[TestClass]
+	public class PronicNumberTests
+	{
+		[TestMethod]
+		public void FixedTest()
+		{
+			Assert.AreEqual(true, Kata.IsPronic(0));
+			Assert.AreEqual(false, Kata.IsPronic(1));
+			Assert.AreEqual(true, Kata.IsPronic(2));
+			Assert.AreEqual(false, Kata.IsPronic(3));
+			Assert.AreEqual(false, Kata.IsPronic(4));
+			Assert.AreEqual(false, Kata.IsPronic(5));
+			Assert.AreEqual(true, Kata.IsPronic(6));
+			Assert.AreEqual(false, Kata.IsPronic(-3));
+			Assert.AreEqual(false, Kata.IsPronic(-27));
+			Assert.AreEqual(true, Kata.IsPronic(100 * (100+1)));
+		}
+	}
+}
