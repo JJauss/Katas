3526877780e212d3d40643a9f571229294fd779e
Joachim Jauß <joachim@jauss-web.de>
2015-12-21 20:35:40 +0100

    SumOfOdNumbers - The clever way

--- NET452Katas/NET452Katas/SumOfOddNumbers/Kata.cs
+++ NET452Katas/NET452Katas/SumOfOddNumbers/Kata.cs
@@ -1,19 +1,11 @@
-﻿namespace NET452Katas.SumOfOddNumbers
+﻿using System;
+
+namespace NET452Katas.SumOfOddNumbers
 {
 	public static class Kata
 	{
-		public static long rowSumOddNumbers(long n)
-		{
-			long currentNumber = 1, sum = 0;
-			for(long currentRow = 0; currentRow < n; currentRow++)
-			{
-				sum = 0;
-				for (long pos = 0; pos < (currentRow +1); currentNumber += 2, pos++)
-				{
-					sum += currentNumber;
-				}
-			}
-			return sum;
+		public static long rowSumOddNumbers(long n) {
+			return (long) Math.Pow(n, 3);
 		}
 	}
 }
