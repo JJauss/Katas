889c7f2dc21c75b4cea7913061a437adab78d490
Joachim Jauß <joachim@jauss-web.de>
2015-12-21 20:34:49 +0100

    SumOfOddNumber

---
+++ NET452Katas/NET452Katas/SumOfOddNumbers/Kata.cs
@@ -0,0 +1,19 @@
+﻿namespace NET452Katas.SumOfOddNumbers
+{
+	public static class Kata
+	{
+		public static long rowSumOddNumbers(long n)
+		{
+			long currentNumber = 1, sum = 0;
+			for(long currentRow = 0; currentRow < n; currentRow++)
+			{
+				sum = 0;
+				for (long pos = 0; pos < (currentRow +1); currentNumber += 2, pos++)
+				{
+					sum += currentNumber;
+				}
+			}
+			return sum;
+		}
+	}
+}
---
+++ NET452Katas/NET452Katas/SumOfOddNumbers/Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NET452Katas.SumOfOddNumbers {
+	[TestClass]
+	public class Test
+	{
+		[TestMethod]
+		public void test1()
+		{
+			Assert.AreEqual(1, Kata.rowSumOddNumbers(1));
+			Assert.AreEqual(8, Kata.rowSumOddNumbers(2));
+			Assert.AreEqual(74088, Kata.rowSumOddNumbers(42));
+		}
+
+	}
+}
